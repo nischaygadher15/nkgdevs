aa060d1788924f9a3d14c0b70f56873decfe540b
nischay gadher <nischaygadher15@gmail.com>
2025-04-03 21:16:51 +0530

    working on modification...GSAP introduced5

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -8,7 +8,7 @@ const Loader = (props) => {
     <div className="absolute top-0 left-0 z-50 w-screen h-screen bg-white flex justify-center items-center">
       <div className="relative flex flex-col justify-between items-center gap-7">
         {/* Logo */}
-        <div className="absolute -top-[170%] text-5xl">
+        <div className="absolute top-0 -translate-y-[calc(100%+20px)] text-5xl">
           <p className="font-bold text-nowrap">{`< NKG/>`}</p>
         </div>
 

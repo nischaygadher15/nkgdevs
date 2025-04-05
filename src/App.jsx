359344500cba56691841dfa6c546827d585c5df8
nischay gadher <nischaygadher15@gmail.com>
2025-04-05 12:20:20 +0530

    working on modification...GSAP introduced smiley animation

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,35 @@ import gsap from "gsap";
 
 function App() {
   let [loading, setLoading] = useState(true);
+  let [pointerTimer, setPointerTime] = useState(5);
+  let [pointerTStatus, setPointerTStatus] = useState(false);
+  let pointerTimer1;
+
+  let counting = async () => {
+    let time = pointerTimer;
+    while (time >= 0) {
+      await new Promise((resolve) => {
+        clearTimeout(pointerTimer1);
+        pointerTimer1 = setTimeout(() => {
+          time--;
+          resolve();
+        }, 1000);
+      });
+    }
+
+    setPointerTStatus(false);
+    hidePointer();
+  };
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1500);
-
-    let pointer = document.querySelector("#pointer");
   }, []);
+
+  useEffect(() => {
+    if (pointerTStatus) {
+      counting();
+    }
+  }, [pointerTStatus]);
 
   let chasePointer = (posx, posy) => {
     gsap.to("#pointer", {
@@ -40,7 +63,7 @@ function App() {
     gsap.to("#pointer", {
       display: "block",
       scale: 1,
-      duration: 1,
+      // duration: 1,
     });
   };
 
@@ -56,14 +79,59 @@ function App() {
           id="mainBody"
           onMouseMove={(e) => {
             chasePointer(e.clientX + 3, e.clientY + 15);
+            setPointerTStatus(true);
+            if (pointerTStatus) {
+              clearTimeout(pointerTimer1);
+              counting();
+            }
+            showPointer();
           }}
           onMouseLeave={() => hidePointer()}
           onMouseEnter={() => showPointer()}
         >
-          <div
+          {/* <============ Pointer ==========> */}
+          {/* simple Dot */}
+          {/* <div id="pointer" className="fixed top-0 left-0 w-5 h-5 bg-[#36B7F0] z-50 rounded-full"></div> */}
+
+          {/* Jumping Smily */}
+          {/* <a
+                  id="pointer"
+                  className="fixed top-0 left-0 z-50"
+                  href="https://www.animatedimages.org/cat-jumping-smileys-and-smilies-669.htm"
+                >
+                  <img
+                    src="https://www.animatedimages.org/data/media/669/animated-jumping-smiley-image-0109.gif"
+                    border="0"
+                    alt="animated-jumping-smiley-image-0109"
+                  />
+                </a> */}
+
+          {/* Red Jumping Smiley */}
+          {/* <a
+                  id="pointer"
+                  className="fixed top-0 left-0 z-50"
+                  href="https://www.animatedimages.org/cat-jumping-smileys-and-smilies-669.htm"
+                >
+                  <img
+                    src="https://www.animatedimages.org/data/media/669/animated-jumping-smiley-image-0040.gif"
+                    border="0"
+                    alt="animated-jumping-smiley-image-0040"
+                  />
+                </a> */}
+
+          {/* Mute Jumping Smily */}
+          <a
             id="pointer"
-            className="fixed top-0 left-0 w-5 h-5 bg-[#36B7F0] z-50 rounded-full"
-          ></div>
+            className="fixed top-0 left-0 z-50"
+            href="https://www.animatedimages.org/cat-jumping-smileys-and-smilies-669.htm"
+          >
+            <img
+              src="https://www.animatedimages.org/data/media/669/animated-jumping-smiley-image-0107.gif"
+              border="0"
+              alt="animated-jumping-smiley-image-0107"
+            />
+          </a>
+
           <header className="w-full bg-white fixed z-30 flex justify-center shadow-md">
             <Navbar />
           </header>
